f80614568bbbee9cf298b483f6e49d3942d35b58
WireBits <159493381+wirebits@users.noreply.github.com>
2024-04-11 15:39:29 +0530

    Add files via upload

---
+++ PowershellDownloader/PowershellDownloader.ino
@@ -0,0 +1,35 @@
+/*
+ * Powershell Downloader
+ * It downloads thing by just URL and save with custom name and extension.
+ * Author - WireBits
+ */
+ 
+#include<Keyboard.h>
+void setup()
+{
+ Keyboard.begin();
+ delay(1000);
+ Keyboard.press(KEY_LEFT_GUI);
+ Keyboard.press('r');
+ Keyboard.releaseAll();
+ delay(1000);
+ Keyboard.print("powershell");
+ delay(1000);
+ Keyboard.press(KEY_RETURN);
+ Keyboard.releaseAll();
+ delay(1000);
+ Keyboard.print("Invoke-WebRequest -Uri \"https://url\" -OutFile \"name.xxx\"");
+ delay(1000);
+ Keyboard.press(KEY_RETURN);
+ Keyboard.releaseAll();
+ delay(3000);
+ Keyboard.print("exit");
+ delay(1000);
+ Keyboard.press(KEY_RETURN);
+ Keyboard.releaseAll();
+ Keyboard.end();
+}
+void loop()
+{
+ //Nothing to do here ;)
+}
---
+++ PowershellDownloader/README.md
@@ -0,0 +1,9 @@
+# Powershell Downloader
+It downloads thing by just URL and save with custom name and extension.
+
+# Use
+- Replace `url` with the actual URL want to download.
+- Replace `name` with the name of the file you want to save.
+- Replace `xxx` with extension of the file you want to save like jpg, png, mp3 *etc*.
+- Compile and upload the code on any supported board you have using Arduino IDE.
+- Done!
